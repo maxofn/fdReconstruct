ef23ae069e0073d89cb84c43d5e5604b6c60a421
Maximilian Ofner <m.ofner@tugraz.at>
2023-12-07 17:07:50 +0100

    Add files via upload

---
+++ FDFM/R/estimate_num_factors.R
@@ -0,0 +1,60 @@
+#' Run 5-fold cross validation for estimating the unknown rank.
+#'
+#' @param M.set   Set on which observations are missing. If M.set = NULL (i.e.
+#'                all observations are available) the rank is chosen such that
+#'                the fraction of variance explained exceeds 0.99.
+#' @param Y.obs   Data matrix.
+#' @param r.max   Maximum number of factors (r.max = 20 by default).
+#'
+#' @return Estimated number of factors.
+#' @export
+#'
+#' @examples
+#' set.seed(123)
+#' data_comp <- GenObs(T = 50, n = 51, r = 4, complete = TRUE)
+#' data_inco <- GenObs(T = 50, n = 51, r = 4, complete = FALSE)
+#' Y0.obs <- rbind(data_inco$Y0.obs, data_comp$Y0.obs)
+#' Y1.obs <- rbind(data_inco$Y1.obs, data_comp$Y1.obs)
+#' RunCV(M.set = 1:10, cbind(Y0.obs, Y1.obs))
+
+RunCV <- function (M.set = NULL, Y.obs, r.max = 20) {
+
+  N <- dim(Y.obs)[2]
+
+  completely.obs <- which(rowSums(is.na(Y.obs)) == 0)
+  Yc <- Y.obs[completely.obs, ]
+  Tc <- dim(Yc)[1]
+
+  r.max <- min(r.max, min(floor(Tc * 4/5), N - length(M.set)) -1)
+
+  if(is.null(M.set)) {
+    ev <- eigen(stats::cov(Yc))$values
+    r.hat <- min(min(which(cumsum(ev)/sum(ev) > 0.99)), r.max)
+  } else {
+    sse <- numeric(r.max)
+    list <- split(1:Tc, factor(sort(rep(1:5, length.out = Tc))))
+
+    for(k in 1:5) {
+      # Run 5-fold cross validation
+      test.obs <- list[[k]]
+      Y.train <- Yc[-test.obs,]
+      Y.test  <- Yc[ test.obs,]
+      Y.dummy <- Yc[ test.obs,]
+      Y.dummy[, M.set] <- NA
+
+      T.train <- dim(Y.train)[1]
+      T.test  <- dim(Y.test )[1]
+
+      for (r in 1:r.max) {
+        reconst <- ReconstFD(rbind(Y.dummy, Y.train), T.set = 1:T.test,
+                             method = 'manual', r = rep(r, T.test),
+                             r.max = r.max)
+        X.hat <- reconst$X.hat
+        sse[r] <- sse[r] + sum((Y.test[, M.set] - X.hat[, M.set])^2)
+      }
+    }
+    r.hat   <- which.min(sse)
+  }
+
+  return(r.hat)
+}
---
+++ FDFM/R/gen_obs.R
@@ -0,0 +1,84 @@
+#' Generate functional data
+#'
+#' @param T Sample size (T = 100 by default).
+#' @param n Number of equispaced grid points (n = 51 by default).
+#' @param r Number of factors (r = 50 by default); must be an even number.
+#' @param type.miss Type of missingness mechanism.
+#' @param ev Decay of eigenvalues; either "exp" (default) or polynomial else.
+#' @param eps.sd Standard deviation of measurement errors.
+#' @param complete If complete = TRUE (default), functions are completely
+#'        observable.
+#'
+#' @returns
+#' A list containing the true data matrices 'X0', 'X1', the noisy data
+#' matrices 'Y0', 'Y1', and the imperfectly observed data matrices
+#' 'Y0.obs', 'Y1.obs' with NA's.
+#'
+#' @export
+#'
+#' @examples
+#' set.seed(123)
+#' data <- GenObs(T = 4, n = 51)
+#' par(mfrow = c(1,2))
+#' matplot(t(data$Y0.obs), type = "l")
+#' matplot(t(data$Y1.obs), type = "l")
+
+GenObs <- function (T = 100, n = 51, r = 50, type.miss = "A", ev = "exp",
+                    eps.sd = 0.1, complete = FALSE) {
+  if (r%%2 == 1) {
+    stop("The parameter r must be even.")
+  }
+  grid <- seq(0, 1, length.out = n)
+  mu <- sin(pi * grid)
+  mu.mat <- matrix(rep(mu, T), byrow = TRUE, nrow = T, ncol = n)
+  b.grid <- as.matrix(1:(r/2), nrow = r/2) %*% t(as.matrix(grid, nrow = n))
+  sin.grid <- sqrt(2) * sin(2 * pi * b.grid)
+  cos.grid <- sqrt(2) * cos(2 * pi * b.grid)
+  r.vec <- 1:(r/2)
+
+  if(ev == "exp") {
+    xi1.sd <- sqrt(exp(- 2 * r.vec + 1))
+    xi2.sd <- sqrt(exp(- 2 * r.vec    ))
+  } else {
+    # polynomially decaying eigenvalues
+    xi1.sd <- sqrt((2 * r.vec - 1)^(-3)/2)
+    xi2.sd <- sqrt((2 * r.vec    )^(-3)/2)
+  }
+
+  xi1 <- matrix(rep(xi1.sd, T), nrow = T, ncol = r/2, byrow = TRUE) *
+    matrix(stats::rnorm(T * r/2), nrow = T, ncol = r/2)
+  xi2 <- matrix(rep(xi2.sd, T), nrow = T, ncol = r/2, byrow = TRUE) *
+    matrix(stats::rnorm(T * r/2), nrow = T, ncol = r/2)
+  X0 <- xi1 %*% sin.grid + xi2 %*% cos.grid + mu.mat
+
+  beta <- 1.1 * t(t(sin.grid[1,])) %*%  sin.grid[1,] +
+          0.7 * t(t(sin.grid[1,])) %*%  cos.grid[1,] +
+          0.5 * t(t(cos.grid[1,])) %*%  sin.grid[1,] +
+          0.3 * t(t(cos.grid[1,])) %*%  cos.grid[1,]
+
+  X1 <- t(beta %*% t(X0) / n)
+
+  Y0 <- X0 + stats::rnorm(length(X0), sd = eps.sd)
+  Y1 <- X1 + stats::rnorm(length(X1), sd = eps.sd)
+
+  Y0.obs <- Y0
+  Y1.obs <- Y1
+
+  if(complete == FALSE) {
+    if (type.miss == "A") {
+      for (t in 1:T) {
+        D <- sample(floor(1 * n/2):floor(3 * n/4), size = 1)
+        Y0.obs[t, D:n] <- NA
+      }
+    }
+    if (type.miss == "B") {
+      for (t in 1:T) {
+        D <- sample(floor(1 * n/4):floor(3 * n/4), size = 1)
+        Y0.obs[t, D:n] <- NA
+      }
+    }
+  }
+
+  return(list(X0 = X0, X1 = X1, Y0 = Y0, Y1 = Y1, Y0.obs = Y0.obs,
+              Y1.obs = Y1.obs))
+}
